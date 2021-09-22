2e38cfc1ccf035e0742feb2d6a2ba34dfe4a61c1
anton <19mann84@gmail.com>
2021-09-22 18:06:22 +0300

    added current build version and abbilety for  display it on dev mode only. fixed problem with styling when have been chosen new version of RN(for iOS tv); cleared all Android cache by using list fo the commansd: From project root: rm -rf ./android/.gradle;
    rm -rf ./android/app/buil;d
    
    rm -rf ~/.gradle/caches;
    
    cd android && ./gradlew clean && cd ..
    
    it is able to solve the mostly parts of problems;

--- src/configs/globalConfig.ts
+++ src/configs/globalConfig.ts
@@ -1,2 +1,5 @@
+import { getVersion, getBuildNumber } from 'react-native-device-info';
 export const SentryDNS =
   'https://1a830de5cb974a1dbd0ed139dd5e6d7d@o936063.ingest.sentry.io/5886190';
+
+export const buildInfo = `Build version: ${getVersion()}; build number: ${getBuildNumber()}. Only for dev mode`;
